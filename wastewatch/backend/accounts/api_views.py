002c90a1d288638976cce9a81d1a2abf090c7e54
pasapak-sayo <johnnaynes2021@gmail.com>
2026-05-30 04:18:56 +0800

    fix(api): fix NameError in register view and syntax in ActivityLog

--- wastewatch/backend/accounts/api_views.py
+++ wastewatch/backend/accounts/api_views.py
@@ -22,6 +22,7 @@ from django.contrib.auth.decorators import login_required
 from rest_framework import viewsets, permissions
 from .models import User, Barangay
 from .serializers import UserSerializer, BarangaySerializer
+from .forms import RegistrationForm
 
 
 class UserViewSet(viewsets.ModelViewSet):
